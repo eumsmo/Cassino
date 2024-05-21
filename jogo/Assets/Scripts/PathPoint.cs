06ef87914c4ff8daafa246a31367d292a6317592
Juan Vitor <juanriven10@gmail.com>
2024-05-21 20:50:38 -0300

    🤏 Pouca coisa
    Quase nada tar

---
+++ jogo/Assets/Scripts/PathPoint.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathPoint : MonoBehaviour {
+    public List<PathPoint> paths = new List<PathPoint>();
+
+    void OnDrawGizmos() {
+        if (paths == null) return;
+
+        for (int i = 0; i < paths.Count; i++) {
+            if (paths[i] == null) continue;
+
+            Vector3 direction = paths[i].transform.position - transform.position;
+            Vector3 position = transform.position + direction / 2;
+
+            Gizmos.color = GetGizmoColor(i);
+
+            Gizmos.DrawLine(transform.position, position);
+        }
+    }
+
+    Color GetGizmoColor(int i) {
+        Color color = Color.white;
+        Color[] colors = { Color.red, Color.green, Color.blue, Color.yellow, Color.magenta};
+
+        if (i < colors.Length) {
+            color = colors[i];
+        }
+
+        return color;
+    }
+}
